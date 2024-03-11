422a7b27876e895b2a7e750a09b03385f9ec1ecc
vixhnuchandran <vixhnuchandran@gmail.com>
2024-03-11 11:06:11 +0530

    chore: Add emoji for user and ai

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import { useChat } from 'ai/react';
+import { useChat } from "ai/react";
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-      {messages.map(m => (
+      {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
-          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.role === "user" ? "👤: " : "🤖: "}
           {m.content}
         </div>
       ))}
